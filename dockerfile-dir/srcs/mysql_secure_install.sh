6330aa46303004b8e7215caeb173cc9a09b005e1
misc <kumo.git@mailnull.com>
2020-07-21 21:12:50 +0200

    j'y comprends pas grand-chose

---
+++ dockerfile-dir/srcs/mysql_secure_install.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+mysql --user=root <<_EOF_
+UPDATE mysql.user SET Password=PASSWORD('${db_root_password}') WHERE User='root';
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+FLUSH PRIVILEGES;
+_EOF_
--- dockerfile-dir/srcs/setup_phpmyadmin.sh
+++ dockerfile-dir/srcs/setup_phpmyadmin.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-apt-get -y install php-fpm php-mysql
-apt-get -y install php-mbstring php-zip php-gd
-apt-get -y install wget
-wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-english.tar.gz
-tar -xzf phpMyAdmin-latest-english.tar.gz
-phpmyadmin_version=`ls | grep -E "phpMyAdmin-[0-9.]+-english"`
-mv $phpmyadmin_version /usr/share/phpmyadmin
+tar -xzf phpMyAdmin-5.0.2-english.tar.gz
+mv phpMyAdmin-5.0.2-english /usr/share/phpmyadmin
+rm phpMyAdmin-5.0.2-english.tar.gz
